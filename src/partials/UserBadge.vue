62c78b9e07f6fe0dfa9e6dc986190424f238b217
IgnetStudio <j.ignatowicz@wit.edu.pl>
2018-02-26 22:14:42 +0100

    minor issues fixed

--- src/partials/UserBadge.vue
+++ src/partials/UserBadge.vue
@@ -1,9 +1,19 @@
 <template>
-    <img src="@/assets/medal.png" alt="">
+ <div class="column is-4 ui__badge-wrapper">
+    <div class="column is-4 ui__badge-active">
+
+       <img src="@/assets/medal.png" alt="">
+    </div>
+ </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
+
+<style lang="scss">
+    .badge-wrapper {
+    //   background: url('@/assets/ui__badge-wrapper.png');
+      display: flex;
+    }
+</style>
